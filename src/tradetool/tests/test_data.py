258e62be930daa89227186a51fcb1b4127cd5a5c
kaspervvliet <kaspervvliet@gmail.com>
2025-04-10 21:01:08 +0200

    Delete src/tradetool directory

--- src/tradetool/tests/test_data.py
+++
@@ -1,9 +0,0 @@
-
-import unittest
-from data import get_latest_data
-
-class TestData(unittest.TestCase):
-    def test_data_structure(self):
-        data = get_latest_data("SOL/USD")
-        self.assertIsInstance(data, list)
-        self.assertTrue(all("timestamp" in d for d in data))
--- src/tradetool/tests/test_telegram.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from telegram_notifier import TelegramNotifier
-
-class TestTelegramNotifier(unittest.TestCase):
-    def test_init(self):
-        bot = TelegramNotifier(bot_token="dummy", chat_id="123")
-        self.assertIsInstance(bot, TelegramNotifier)
