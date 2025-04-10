0d256c211f75d3403707ece74e7a1f280de3fdc9
kaspervvliet <kaspervvliet@gmail.com>
2025-04-10 20:41:05 +0200

    Delete aion_render_final_working directory

--- aion_render_final_working/launcher_patch_v4_3.py
+++
@@ -1,9 +0,0 @@
-from dynamic_strategy_morpher import DynamicStrategyMorpher
-from strategy_switcher import StrategySwitcher
-import json
-
-def adapt_config_with_morphing(config, bias):
-    switcher = StrategySwitcher(config)
-    config = switcher.adapt_strategy_to_bias(bias)
-    morpher = DynamicStrategyMorpher(bias)
-    return morpher.morph(config)
