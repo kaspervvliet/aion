565e56496e7d4fa210389da1c5f8fd3b718bdf3b
kaspervvliet <kaspervvliet@gmail.com>
2025-04-11 17:42:01 +0200

    Delete src/tradetool/ui_components/whynot_button.py

--- src/tradetool/ui_components/whynot_button.py
+++
@@ -1,7 +0,0 @@
-
-import streamlit as st
-
-def show_whynot(reason_list):
-    st.markdown("### ❌ Waarom deze entry niet is uitgevoerd:")
-    for reason in reason_list:
-        st.markdown(f"- {json.dumps(reason, indent=2)}")
